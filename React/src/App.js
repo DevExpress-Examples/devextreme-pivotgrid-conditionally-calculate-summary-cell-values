4396f7e9729e203bbdba9756c82dec660c155add
Luis Capuyon <frostehhh967@gmail.com>
2020-09-22 17:18:22 +0800

    Updated React Approach

--- React/src/App.js
+++ React/src/App.js
@@ -17,13 +17,7 @@ const pivotGridRef = React.createRef();
 
 function App() {
   const [isConditional, setIsConditional] = useState(true)
-  // let isConditional2 = useMemo(() => isConditional, [isConditional])
-
-  const calculateCustomSummary = useMemo(() => {
-    debugger;
-    return options => salesCalculateCustomSummary(options, isConditional)
-  }, [isConditional]);
-
+  
   const dataSource = useMemo(() => { 
     return {
       fields: [{
@@ -50,7 +44,6 @@ function App() {
         format: 'currency',
         area: 'data',
         calculateCustomSummary: calculateCustomSummary
-        // calculateCustomSummary: (options) => salesCalculateCustomSummary(options, isConditional)
       },{
         caption: "Approved",
         dataField: "isApproved",
@@ -59,91 +52,37 @@ function App() {
       }],
       store: sales
     }
-  }, [])
-
-
-
-  // const dataSource = {
-  //   fields: [{
-  //     caption: 'Region',
-  //     width: 120,
-  //     dataField: 'region',
-  //     area: 'row',
-  //     expanded: true
-  //   }, {
-  //     caption: 'City',
-  //     dataField: 'city',
-  //     width: 150,
-  //     area: 'row',
-  //     selector: citySelector
-  //   }, {
-  //     dataField: 'date',
-  //     dataType: 'date',
-  //     area: 'column',
-  //     expanded: true
-  //   }, {
-  //     caption: 'Sales',
-  //     dataType: 'number',
-  //     summaryType: 'custom',
-  //     format: 'currency',
-  //     area: 'data',
-  //     calculateCustomSummary: calculateCustomSummary
-  //     // calculateCustomSummary: (options) => salesCalculateCustomSummary(options, isConditional)
-  //   },{
-  //     caption: "Approved",
-  //     dataField: "isApproved",
-  //     summaryType: "sum",
-  //     area: "data"
-  //   }],
-  //   store: sales
-  // }
+  }, [isConditional])
     
-  // function calculateCustomSummary(options) {
-  //   switch (options.summaryProcess) {
-  //       case "start":
-  //           options.totalValue = { conditionalVal: 0, rawVal: 0, count: 0 };
-  //           break;
-  //       case "calculate":
-  //           options.totalValue.count += 1;
-  //           options.totalValue.rawVal += options.value.amount;
-  //           if (options.value.isApproved) {
-  //               options.totalValue.conditionalVal += options.value.amount;
-  //           }
-  //           break;
-  //       case "finalize":
-  //           if (options.totalValue.count === 1 || !isConditional) {
-  //               // If not conditional, this block will always be run
-  //               options.totalValue = options.totalValue.rawVal;
-  //           } else {
-  //               options.totalValue = options.totalValue.conditionalVal;
-  //           }
-  //   }
-  // }
-
-  function onValueChanged({value}) {
-    
-    setIsConditional(value)
-    let ds = pivotGridRef.current.instance.getDataSource()
-    // isConditional  = value
-    ds.reload()
+  function calculateCustomSummary(options) {
+    switch (options.summaryProcess) {
+        case "start":
+            options.totalValue = { conditionalVal: 0, rawVal: 0, count: 0 };
+            break;
+        case "calculate":
+            options.totalValue.count += 1;
+            options.totalValue.rawVal += options.value.amount;
+            if (options.value.isApproved) {
+                options.totalValue.conditionalVal += options.value.amount;
+            }
+            break;
+        case "finalize":
+            if (options.totalValue.count === 1 || !isConditional) {
+                // If not conditional, this block will always be run
+                options.totalValue = options.totalValue.rawVal;
+            } else {
+                options.totalValue = options.totalValue.conditionalVal;
+            }
+    }
   }
 
-  const pivotGridRender = useMemo(() => <PivotGrid id="sales"
-      ref={pivotGridRef}
-      allowSortingBySummary={true}
-      allowSorting={true}
-      allowFiltering={true}
-      allowExpandAll={true}
-      height={440}
-      showBorders={true}
-      dataSource={dataSource}>
-      <FieldChooser enabled={false} />
-    </PivotGrid>,[])
+  function onValueChanged({value}) {
+    setIsConditional(value)
+  }
 
   return (
     <div className="App">
       <div className="long-title"><h3>Sales Amount by Region</h3></div>
-      {/* {pivotGridRender} */}
       <PivotGrid id="sales"
         ref={pivotGridRef}
         allowSortingBySummary={true}
@@ -173,26 +112,4 @@ export default App;
 
 function citySelector(data) {
   return data.city + ' (' + data.country + ')';
-}
-
-function salesCalculateCustomSummary(options, isConditional) {
-  switch (options.summaryProcess) {
-    case "start":
-        options.totalValue = { conditionalVal: 0, rawVal: 0, count: 0 };
-        break;
-    case "calculate":
-        options.totalValue.count += 1;
-        options.totalValue.rawVal += options.value.amount;
-        if (options.value.isApproved) {
-            options.totalValue.conditionalVal += options.value.amount;
-        }
-        break;
-    case "finalize":
-        if (options.totalValue.count === 1 || !isConditional) {
-            // If not conditional, this block will always be run
-            options.totalValue = options.totalValue.rawVal;
-        } else {
-            options.totalValue = options.totalValue.conditionalVal;
-        }
-}
 }
